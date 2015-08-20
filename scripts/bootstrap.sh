f7403f2d1683e35f9751e5aa1d2d63afa2f2f426
Roberto Acevedo <rxacevedo@fastmail.com>
2015-08-20 19:17:27 -0400

    Update bootstrap scripts to update cfg/run DDL

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,2 +1,4 @@
 sed -i "s/localhost/$PGHOST/g" $CATALINA_HOME/webapps/pentaho/META-INF/context.xml
 sed -i "s/localhost/$PGHOST/g" $PENTAHO_HOME/server/biserver-ee/pentaho-solutions/system/jackrabbit/repository.xml
+sed -i "s/localhost/$PGHOST/g" $PENTAHO_HOME/server/biserver-ee/pentaho-solutions/system/hibernate/postgresql.hibernate.cfg.xml
+sh setup_postgres.sh
