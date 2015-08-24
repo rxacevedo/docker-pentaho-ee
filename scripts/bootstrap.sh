fb97e46cf85f169b2607a0897cbdd8442ca82e2a
Roberto Acevedo <rxacevedo@fastmail.com>
2015-08-24 12:55:23 -0400

    Update bootstrap with multi-line sed replacement for solution-path

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,6 @@
 sed -i "s/localhost/$PGHOST/g" $CATALINA_HOME/webapps/pentaho/META-INF/context.xml
 sed -i "s/localhost/$PGHOST/g" $PENTAHO_HOME/server/biserver-ee/pentaho-solutions/system/jackrabbit/repository.xml
 sed -i "s/localhost/$PGHOST/g" $PENTAHO_HOME/server/biserver-ee/pentaho-solutions/system/hibernate/postgresql.hibernate.cfg.xml
+# This one was fun to figure out...
+sed -i '/<param-name>solution-path<\/param-name>.*/ {N; s#\(<param-name>solution-path<\/param-name>\).*<\/param-value>#\1\n\t\t<param-value>'"${PENTAHO_HOME}server/biserver-ee/pentaho-solutions"'<\/param-value>#}' $CATALINA_HOME/webapps/pentaho/WEB-INF/web.xml
 sh setup_postgres.sh
