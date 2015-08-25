d749e58186d0ce134992e4ea22e5fab1174e2f46
Roberto Acevedo <rxacevedo@fastmail.com>
2015-08-25 08:23:40 -0400

    Update bootstrap script, comment some lines in web.xml

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,9 @@
 sed -i "s/localhost/$PGHOST/g" $CATALINA_HOME/webapps/pentaho/META-INF/context.xml
 sed -i "s/localhost/$PGHOST/g" $PENTAHO_HOME/server/biserver-ee/pentaho-solutions/system/jackrabbit/repository.xml
 sed -i "s/localhost/$PGHOST/g" $PENTAHO_HOME/server/biserver-ee/pentaho-solutions/system/hibernate/postgresql.hibernate.cfg.xml
-# This one was fun to figure out...
 sed -i '/<param-name>solution-path<\/param-name>.*/ {N; s#\(<param-name>solution-path<\/param-name>\).*<\/param-value>#\1\n\t\t<param-value>'"${PENTAHO_HOME}server/biserver-ee/pentaho-solutions"'<\/param-value>#}' $CATALINA_HOME/webapps/pentaho/WEB-INF/web.xml
+# Ok fine I'll use perl
+perl -0777 -i -pe 's/(<!-- \[BEGIN HSQLDB DATABASES\] -->.*<!-- \[END HSQLDB DATABASES\] -->)/<!--\n    $1\n    -->/smg' $CATALINA_HOME/webapps/pentaho/WEB-INF/web.xml
+perl -0777 -i -pe 's/(<!-- \[BEGIN HSQLDB STARTER\] -->.*<!-- \[END HSQLDB STARTER\] -->)/<!--\n    $1\n    -->/smg' $CATALINA_HOME/webapps/pentaho/WEB-INF/web.xml
+rm -f $PENTAHO_HOME/server/biserver-ee/pentaho-solutions/system/default-content/*.zip
 sh setup_postgres.sh
--- scripts/setup_postgres.sh
+++ scripts/setup_postgres.sh
@@ -4,4 +4,4 @@ psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/server/biserver-ee/da
 psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/server/biserver-ee/data/postgresql/create_repository_postgresql.sql
 psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/server/biserver-ee/data/postgresql/create_quartz_postgresql.sql
 # psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/server/biserver-ee/data/postgresql/pentaho_logging_postgresql.sql
-psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/server/biserver-ee/data/postgresql/pentaho_mart_postgresql.sql
+# psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/server/biserver-ee/data/postgresql/pentaho_mart_postgresql.sql
